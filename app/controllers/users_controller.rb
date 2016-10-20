3526beecd011583c234ba1be9f322e3d6907118d
Nguyen Van Duc <nguyen.van.ducb@framgia.com>
2016-10-20 13:15:51 +0700

    Finish user signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,26 @@
 class UsersController < ApplicationController
-  def new
+
+  def show
+    @user = User.find_by id: params[:id]
   end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new user_params
+    if @user.save
+      flash[:success] = t ".success"
+      redirect_to @user
+    else
+      render "new"
+    end
+  end
+
+  private
+    def user_params
+      params.require(:user).permit :name, :email, :password,
+        :password_confirmation
+    end
 end
