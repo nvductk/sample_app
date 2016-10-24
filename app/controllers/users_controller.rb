e756859f6db2359aca5defb1b636f918184bbef8
Nguyen Van Duc <nguyen.van.ducb@framgia.com>
2016-10-24 13:31:53 +0700

    Implement basic login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,10 +11,11 @@ class UsersController < ApplicationController
   def create
     @user = User.new user_params
     if @user.save
+      log_in @user
       flash[:success] = t ".success"
       redirect_to @user
     else
-      render "new"
+      render :new
     end
   end
 
