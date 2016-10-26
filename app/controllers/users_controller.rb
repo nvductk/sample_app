3cbb1ace3f65824cd1e7a30ab22b23ba9b34a435
Nguyen Van Duc <nguyen.van.ducb@framgia.com>
2016-10-26 10:53:13 +0700

    Add account activation

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -22,9 +22,9 @@ class UsersController < ApplicationController
   def create
     @user = User.new user_params
     if @user.save
-      log_in @user
-      flash[:success] = t ".success"
-      redirect_to @user
+      @user.send_activation_email
+      flash[:info] = t (".Please_check")
+      redirect_to root_url
     else
       flash[:danger] = t ".error"
       render :new
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-
+  get "account_activations/edit"
   get "sessions/new"
 
   root "static_pages#home"
@@ -11,6 +11,7 @@ Rails.application.routes.draw do
   post "/login", to: "sessions#create"
   delete "/logout", to: "sessions#destroy"
   resources :users
+  resources :account_activations, only: [:edit]
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
